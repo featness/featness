3c653b2efd883945e54df3a11e2f1fdacfafd53b
Rafael Floriano da Silva <rflorianobr@gmail.com>
2014-04-01 15:28:52 -0300

    Test loadConfig method

--- api-server/main_test.go
+++ api-server/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/maraino/go-mock"
 	"launchpad.net/gocheck"
 	"net/http"
 	"github.com/tsuru/config"
@@ -11,16 +12,16 @@ import (
 // Hook up gocheck into the "go test" runner.
 func Test(t *testing.T) { gocheck.TestingT(t) }
 
-type Suite struct {
-}
+type Suite struct {}
 
 var _ = gocheck.Suite(&Suite{})
 
-func (s *Suite) TestLoadConfig(c *gocheck.C) {
-	loadConfigFile("../testdata/etc/featness-api1.conf")
-	value, err := config.GetBool("my_data")
-	c.Assert(value, gocheck.Equals, true)
-	c.Assert(err, gocheck.IsNil)
+type LoggerTest struct {
+    mock.Mock
+}
+
+func (l *LoggerTest) Panicf(format string, v ...interface{}) {
+    l.Called(format, )
 }
 
 func routeExists(method string, url string) bool {
@@ -35,4 +36,31 @@ func routeExists(method string, url string) bool {
 
 func (s *Suite) TestRouterHasHealthcheck(c *gocheck.C) {
 	c.Assert(routeExists("GET", "/healthcheck"), gocheck.Equals, true)
+}
+
+func (s *Suite) TestLoadConfig(c *gocheck.C) {
+	logger := &LoggerTest{}
+	logger.When("Panicf").Times(0)
+
+	loadConfigFile("../testdata/etc/featness-api1.conf", logger)
+	
+	value, errorGetBool := config.GetBool("my_data")
+	ok, errorMock := logger.Verify()
+	
+	c.Assert(value, gocheck.Equals, true)
+	c.Assert(errorGetBool, gocheck.IsNil)
+	c.Assert(ok, gocheck.Equals, true)
+	c.Assert(errorMock, gocheck.IsNil)
+}
+
+func (s *Suite) TestLoadConfigWhenWrongPath(c *gocheck.C) {
+	logger := &LoggerTest{}
+	logger.When("Panicf", `Could not find featness-api config file. Searched on %s.
+	For an example conf check featness-api/etc/featness-api.conf file.\n %s`).Times(1)
+
+	loadConfigFile("wrong-path.conf", logger)
+
+	ok, errorMock := logger.Verify()
+	c.Assert(ok, gocheck.Equals, true)
+	c.Assert(errorMock, gocheck.IsNil)
 }
