fdb527b6e10e5933c69aa8f8946a94d0916458c7
Bernardo Heynemann <heynemann@gmail.com>
2014-04-06 23:03:35 -0300

    Tests working properly now that we have an user struct

--- api/models.go
+++ api/models.go
@@ -3,11 +3,26 @@ package api
 import (
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
+	"time"
 )
 
+type User struct {
+	Id       bson.ObjectId `json:"id"        bson:"_id,omitempty"`
+	Name     string        `json:"name"`
+	UserID   string        `json:"userid"`
+	JoinedAt time.Time     `json:"joinedat"`
+	ImageURL string        `json:"imageurl"`
+}
+
 type Team struct {
-	Name    string
-	Members []string
+	Id      bson.ObjectId   `json:"id"        bson:"_id,omitempty"`
+	Name    string          `json:"name"`
+	Members []bson.ObjectId `json:"members"`
+}
+
+func Users() *mgo.Collection {
+	conn, _ := Conn()
+	return conn.C("users")
 }
 
 func Teams() *mgo.Collection {
@@ -15,7 +30,24 @@ func Teams() *mgo.Collection {
 	return conn.C("teams")
 }
 
-func GetTeamsFor(member string) ([]Team, error) {
+func GetOrCreateUser(name string, userID string, imageURL string) (*User, error) {
+	usersColl := Users()
+
+	_, err := usersColl.Upsert(
+		bson.M{"userid": userID},
+		&User{bson.NewObjectId(), name, userID, time.Now(), imageURL},
+	)
+
+	user := &User{}
+	err = usersColl.Find(bson.M{"userid": userID}).One(user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func GetTeamsFor(member bson.ObjectId) ([]Team, error) {
 	teams := []Team{}
 	err := Teams().Find(bson.M{"members": member}).All(&teams)
 
--- api/models_test.go
+++ api/models_test.go
@@ -5,48 +5,88 @@ import (
 	. "github.com/onsi/gomega"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
+	"log"
+	"time"
 )
 
-var _ = Describe("Team", func() {
+var _ = Describe("Models", func() {
 	var (
-		teams *mgo.Collection
+		teams     *mgo.Collection
+		users     *mgo.Collection
+		testUsers []User
 	)
 
 	BeforeEach(func() {
+		testUsers = []User{}
 		teams = Teams()
 		teams.RemoveAll(bson.M{})
+
+		users = Users()
+		users.RemoveAll(bson.M{})
+
+		for i := 0; i < 10; i++ {
+			userID := "userID" + string(i)
+			users.Insert(
+				&User{bson.NewObjectId(), "User " + string(i), userID, time.Now(), "http://picture.com/" + string(i)},
+			)
+			result := User{}
+			err := users.Find(bson.M{"userid": userID}).One(&result)
+			if err != nil {
+				log.Panicf(err.Error())
+			}
+			testUsers = append(testUsers, result)
+		}
 	})
 
-	Context("when the user is in a team", func() {
-		It("Can get all teams for a given member", func() {
-			err := teams.Insert(
-				&Team{"test1-team1", []string{"heynemann"}},
-				&Team{"test1-team2", []string{"john"}},
-			)
-			Expect(err).ShouldNot(HaveOccurred())
+	Context(" - User model", func() {
+		Context("when the user is logging in for the first time", func() {
+			It("Should create a new user and return it", func() {
+				user, err := GetOrCreateUser("Bernardo Heynemann", "heynemann", "http://my.picture.url")
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(user).ShouldNot(BeNil())
+				Expect(user.Name).Should(Equal("Bernardo Heynemann"))
+				Expect(user.UserID).Should(Equal("heynemann"))
+				Expect(user.ImageURL).Should(Equal("http://my.picture.url"))
+			})
+		})
 
-			userTeams, err := GetTeamsFor("heynemann")
-			Expect(err).ShouldNot(HaveOccurred())
+		Context("when the user is logging again", func() {
 
-			Expect(userTeams).Should(HaveLen(1))
-			Expect(userTeams[0].Name).Should(Equal("test1-team1"))
-			Expect(userTeams[0].Members).Should(HaveLen(1))
-			Expect(userTeams[0].Members[0]).Should(Equal("heynemann"))
 		})
 	})
 
-	Context("when the user is in no teams", func() {
-		It("should return an empty list of teams", func() {
-			err := teams.Insert(
-				&Team{"test2-team1", []string{"jane"}},
-				&Team{"test2-team2", []string{"john"}},
-			)
-			Expect(err).ShouldNot(HaveOccurred())
+	Context(" - Team model", func() {
+		Context("when the user is in a team", func() {
+			It("Can get all teams for a given member", func() {
+				err := teams.Insert(
+					&Team{bson.NewObjectId(), "test1-team1", []bson.ObjectId{testUsers[0].Id}},
+					&Team{bson.NewObjectId(), "test1-team2", []bson.ObjectId{testUsers[1].Id}},
+				)
+				Expect(err).ShouldNot(HaveOccurred())
 
-			userTeams, err := GetTeamsFor("heynemann")
-			Expect(err).ShouldNot(HaveOccurred())
+				userTeams, err := GetTeamsFor(testUsers[0].Id)
+				Expect(err).ShouldNot(HaveOccurred())
 
-			Expect(userTeams).Should(BeEmpty())
+				Expect(userTeams).Should(HaveLen(1))
+				Expect(userTeams[0].Name).Should(Equal("test1-team1"))
+				Expect(userTeams[0].Members).Should(HaveLen(1))
+				Expect(userTeams[0].Members[0]).Should(Equal(testUsers[0].Id))
+			})
+		})
+
+		Context("when the user is in no teams", func() {
+			It("should return an empty list of teams", func() {
+				err := teams.Insert(
+					&Team{bson.NewObjectId(), "test2-team1", []bson.ObjectId{testUsers[2].Id}},
+					&Team{bson.NewObjectId(), "test2-team2", []bson.ObjectId{testUsers[3].Id}},
+				)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				userTeams, err := GetTeamsFor(testUsers[4].Id)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				Expect(userTeams).Should(BeEmpty())
+			})
 		})
 	})
 
